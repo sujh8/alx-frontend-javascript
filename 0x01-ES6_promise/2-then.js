054ce9ff7b00c5042f9e4438cb18a2db3de75e9f
sujh8 <suhajhm@yahoo.com>
2024-06-26 11:56:03 +0000

    java

---
+++ 0x01-ES6_promise/2-then.js
@@ -0,0 +1,13 @@
+export default function handleResponseFromAPI(promise) {
+    return promise
+      .then(() => ({ status: 200, body: 'success' }))
+      .catch(() => new Error())
+      .finally(() => console.log('Got a response from the API'));
+  }
+  export default function handleResponseFromAPI(promise) {
+    return promise
+      .then(() => ({ status: 200, body: 'success' }))
+      .catch(() => new Error())
+      .finally(() => console.log('Got a response from the API'));
+  }
+  
